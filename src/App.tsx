af7db1f2d8d5eafe69c25c7eb1199afcf9a26b80
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-08-01 18:52:13 -0400

    TaskList basically done

--- src/App.tsx
+++ src/App.tsx
@@ -12,35 +12,39 @@ type Task = {
   description: string
 }
 
-type TaskListProps = {
-  tasks: Task[]
-}
 
 
-function TaskList({tasks}: TaskListProps){
+function TaskList(){
+  const [tasks,setTasks] = useState([{title:"Test",description:"description",id:1,done:false},{title:"Sweep the Kitchen",description:"Get under the cabinets, do a good job",id:2,done:false},{title:"Another one!",description:"Do it!",id:3,done:false}])
+  function handleCheckChange(taskId:number){
+    const newTasks = [...tasks]
+    let changedTask = newTasks.find(t => t.id === taskId)
+    changedTask!.done = !changedTask!.done
+    newTasks.sort((a,b) => Number(b.done)-Number(a.done))
+    setTasks(newTasks)
+  }
  console.log("tasks",tasks)
- const allTasks = tasks.map((task) => <Task key = {task.id} title={task.title} description = {task.description}/>)
+ const allTasks = tasks.map((task) => <Task key = {task.id} title={task.title} description = {task.description} status={task.done} handleChange={() => handleCheckChange(task.id)}/>)
  console.log("all",allTasks)
- return <>
- <h1>Task list</h1>
- <h3>Sorted by completion</h3>
+ return <div className='flex flex-col'>
+<div className='text-4xl'>Task list</div>
+ <div className='text-xs'>Sorted by completion</div>
 {allTasks}
-test
- </>
+ </div>
+ 
 }
 
-type TaskProps = {title:string,description:string}
+type TaskProps = {title:string,description:string,status:boolean,handleChange:Function}
 
-function Task ({ title, description }: TaskProps) {
+function Task ({ title, description,status,handleChange}: TaskProps) {
   // todo: state done
-  const [done,setDone] = useState(false) //bg-[#E2FFE5]
-  const colorString = done ? 'bg-[#E2FFE5]' : 'bg-white' 
-  console.log(done,colorString)
-  const twString = `flex flex-row py-2 pr-8 ${colorString} w-[95%] max-w-md rounded-[0.7em] border border-gray-200`
+  const colorString = status ? 'bg-[#E2FFE5]' : 'bg-white' 
+  console.log(status,colorString)
+  const twString = `flex flex-row py-2 pr-8 ${colorString} w-[95%] max-w-md rounded-[0.7em] border border-gray-200 mb-4`
   return (
     <div className={twString}>
         <div className='flex flex-col justify-center'>
-        <input checked={done} onChange={()=>setDone(!done)} type="checkbox" className='appearance-none w-5 h-5 bg-white border border-gray-200 mx-5 checked:bg-[#359845] rounded-[0.25rem]' />
+        <input checked={status} onChange={()=>handleChange()} type="checkbox" className='appearance-none w-5 h-5 bg-white border border-gray-200 mx-5 checked:bg-[#359845] rounded-[0.25rem]' />
         </div>
         <div className='flex flex-col items-start justify-center'>
           <div className='text-md sm:text-lg'>
@@ -55,17 +59,17 @@ function Task ({ title, description }: TaskProps) {
 }
 
 function App() {
-  const tasks: Task[] = [{title:"Test",description:"description",id:1},{title:"Sweep the Kitchen",description:"Get under the cabinets, do a good job",id:2},{title:"Another one!",description:"Do it!",id:3}]
+
+  //: Task[]
+
   return (
     <>
-    <div className='bg-red'>
     <div className='w-full min-h-full flex flex-col items-center'>
-    <TaskList tasks={tasks}/>
+    <TaskList />
     <div className='my-2'></div>
     <div className='my-2'></div>
     </div>
       
-    </div>
     </>
   )
 }
